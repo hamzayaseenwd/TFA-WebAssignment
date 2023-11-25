d61bc88ddc3454e9d5b2b5d1c3074988f1b1d1f5
hamzayaseenwd <hamzayaseen836@gmail.com>
2023-11-25 21:52:28 +0500

    Update

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ var cardObj = [
   // console.log(cardObj);
   
   var cardCon = document.getElementById("cardContent");
-  
   for (var i = 0; i < cardObj.length; i++) {
     var image = cardObj[i].image;
     var title = cardObj[i].title;
@@ -152,3 +151,83 @@ var cardObj = [
     localStorage.setItem("cartData", JSON.stringify(arr));
   }
   
+// nav start
+
+var navBar = document.getElementById("navBar")
+
+// nav End
+
+    // footer start
+var footer = document.getElementById("footer")
+footer.innerHTML += `<main class="container">
+<hr>
+<section class="row">
+<div class="col-md-6">
+<div class='m-2'>
+<h2>
+School Address <br>
+Head Office:
+            </h2>
+            <p><i class="fa-solid fa-location-dot"></i>Head Office: B-7, Yousuf Plaza, Block 16, F.B Area, Karachi.</p>
+            
+            <p>
+            <a href="tel:000000000" class="text-white text-decoration-none">
+
+            
+                    <i class="fa-solid fa-phone"></i> +92 331-3432382
+                    </a>
+            </p>
+
+            <p>
+            <a href="mailto:someone@gmail.com" class="text-white text-decoration-none">
+
+                    <i class="fa-solid fa-envelope"></i> sales@yourcompany.com
+                </a>
+                </p>
+        </div>
+    </div>
+    <div class="col-md-6">
+    <div class="">
+            <h2>
+                School Address <br>
+                Branch Office:
+            </h2>
+            <p><i class="fa-solid fa-location-dot"></i>Branch: A-61, Yousuf Plaza, Block 16, F.B Area, Karachi.</p>
+            <p>
+            <a href="tel:000000000" class="text-white text-decoration-none">
+            
+            
+                    <i class="fa-solid fa-phone"></i> +92 331-3432382
+                    </a>
+                    </p>
+            <a href="" class="text-white fs-3"><i class="fa-brands fa-facebook"></i></a>
+            <a href="" class="text-white fs-3"> <i class="fa-brands fa-instagram"></i></a>
+            <a href="">
+                <!-- <img src="images/icons/app-icon-1.png" alt="">
+            </a>
+            <a href="">
+                <img src="images/icons/app-icon-2.png" alt=""> -->
+                </a>
+        </div>
+        </div>
+</section>
+<br>
+<hr>
+<section class="row m-0">
+    <div class="col-md-4">
+    <div class="d-flex justify-content-around">
+            
+        </div>
+    </div>
+    <div class="col-md-4">
+    <div class="mt-2 m-0">
+    <p><a href="" class="text-white text-decoration-none">Design by: Muhammad Hamza Siddiqui</a></p>
+    </div>
+    </div>
+    
+    </section>
+    </main>`
+    
+    
+    
+    // footer End
